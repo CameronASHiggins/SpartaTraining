d43864bad0498e267ac0a7eb7a1fec3855ac62e6
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2022-11-28 13:27:33 +0000

    Created the README for mondays work, design patterns.

--- EnumTask/src/main/java/com/sparta/SolarSystem.java
+++ EnumTask/src/main/java/com/sparta/SolarSystem.java
@@ -1,8 +1,18 @@
 package com.sparta;
 
+import java.util.Arrays;
+
 public class SolarSystem {
     public static void main(String[] args) {
         Planet Earth = Planet.EARTH;
+        Planet Planets[] = Planet.values();
+        System.out.println(Arrays.toString(Planets));
+        Planet Mars = Planet.MARS;
+        if(!(Earth.getPlanetName() == Mars.getPlanetName())){
+            System.out.println("Earth is not named Mars");
+        }
+
+        System.out.println(2.0 == 2);
 
         System.out.println(Earth.getPlanetName() + " is " + Earth.getPlanetDistanceFromSol() + "*10^6 km from the Sun and has a mass of " + Earth.getPlanetMass() + "*10^24 kg" );
     }
