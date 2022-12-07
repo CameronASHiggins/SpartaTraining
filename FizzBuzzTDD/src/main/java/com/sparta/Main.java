681d13f95146686a78b37a7cd4e7f1eb70852e04
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2022-12-07 11:05:37 +0000

    Adding Unversioned files

---
+++ FizzBuzzTDD/src/main/java/com/sparta/Main.java
@@ -0,0 +1,10 @@
+package com.sparta;
+
+public class Main {
+    public static void main(String[] args) {
+        int max = 50;
+        for (int i = 1; i<= max; i++){
+            System.out.println(FizzBuzz.fizzBuzz(i));
+        }
+    }
+}
---
+++ JavaIO/src/main/java/com/sparta/FileDriver.java
@@ -0,0 +1,10 @@
+package com.sparta;
+
+public class FileDriver {
+    public static void main(String[] args) {
+        //C:\Users\camer\Sparta\JavaIO\sort-manager.txt
+//        TextFileIO.readFile("EmployeeRecords.csv");
+//        CsvFileIO.readWriteFile("EmployeeRecords.csv","EmployeeRecords2.csv");
+        BinaryFileIO.readBinary("KnitGelt.jpg");
+    }
+}
