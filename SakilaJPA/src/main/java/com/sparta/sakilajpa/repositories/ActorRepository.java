bc613bae4ec84e891da4fa61a2846d781933c399
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2023-01-04 09:31:32 +0000

    Added http mappings to SakilaJPA

--- SakilaJPA/src/main/java/com/sparta/sakilajpa/repositories/ActorRepository.java
+++ SakilaJPA/src/main/java/com/sparta/sakilajpa/repositories/ActorRepository.java
@@ -2,9 +2,17 @@ package com.sparta.sakilajpa.repositories;
 
 import com.sparta.sakilajpa.entities.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
+    @Transactional
+    @Modifying
+    @Query("update Actor a set a.firstName = ?1, a.lastName = ?2, a.lastUpdate = ?3 where a.id = ?4")
+    int updateFirstNameAndLastNameAndLastUpdateById(String firstName, String lastName, Instant lastUpdate, Integer id);
     List<Actor> findByLastName(String lastName);
 }
